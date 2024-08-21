0b1cfca120a72098b129b24d40cfe06212e5a7bb
sushantpadha <24b1057@iitb.ac.in>
2024-08-21 12:30:42 +0530

    add code for D4 lab 3

--- temp.cpp
+++ temp.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include <climits>
 using namespace std;
 
 int main() {
-    int i = 0;
-    while(i<10){
-	    cout << (++i) << " " << i << " " << (i++) << endl;
-	}
+    ios_base::sync_with_stdio(false);  // prevents sync C++ streams w C streams
+    cin.tie(NULL);                     // disables flushing of cout before reading cin
+
+    // unsigned int n;
+    // char c;
+    // c = -127;
+    // n = c;
+    // cout << n << ';' << c << '\n';
+    // cout << true + UINT32_MAX + c << '\n';
+
+    unsigned int n;
+    cin >> n;
+    cout << n << '\n';
+
+    return 0;
 }
