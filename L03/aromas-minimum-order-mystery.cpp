ef48fb366eed4d73f5576bfba182efc794b3c408
sushantpadha <24b1057@iitb.ac.in>
2024-08-24 23:48:27 +0530

    fix and optimize L03 code

--- L03/aromas-minimum-order-mystery.cpp
+++ L03/aromas-minimum-order-mystery.cpp
@@ -1,19 +1,18 @@
 #include <simplecpp>
 
 main_program {
+    // --------- LOGIC ---------
+    // loop { if LSB == 1, count++; i++
+    //        right shift p by one }
+    // i = position of bit being read (LSB is 0)
+    // exit when i=12
+    // add the remaining amount as p*2 or p<<1
     unsigned int p;
     cin >> p;
-    unsigned int one = 1;
     int count = 0;
-    for (int i = 0; p > 0; p >>= 1, i++) {
-        if (p & one) {
-            if (i < 12)
-                count++;
-            else {
-                int j = i - 11;
-                count += pow(2, j);
-            }
-        }
+    for (int i = 0; p > 0 && i < 12 ; p >>= 1, i++) {
+        if (p & 1U) count++;
     }
+    count += (p << 1);
     cout << count << endl;
 }
