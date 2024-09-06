8af8b13d62d44353c695537448313aebb236cbca
sushantpadha <24b1057@iitb.ac.in>
2024-09-06 21:47:14 +0530

    fix code for lab 5 p1

--- L05/quadrilaterals.cpp
+++ L05/quadrilaterals.cpp
@@ -20,15 +20,16 @@ int main() {
         return 0;
     }
 
-    int d1 = (x1-x3)*(x1-x3) + (y1-y3)*(y1-y3);
-    int d2 = (x2-x4)*(x2-x4) + (y2-y4)*(y2-y4);
-    if(d1 == d2) {
+    // if diagonals equal and diagonal midpoints coincide then rectangle
+    int d1 = sqdistance(x1,x3,y1,y3);
+    int d2 = sqdistance(x2,x4,y2,y4);
+    if((d1 == d2) && (x1 + x3 == x2 + x4) && (y1 + y3 == y2 + y4)) {
         int a1 = sqdistance(x1,x2,y1,y2);
         int a2 = sqdistance(x2,x3,y2,y3);
         int a3 = sqdistance(x3,x4,y3,y4);
         int a4 = sqdistance(x4,x1,y4,y1);
 
-        // only 2 are required but writing 3 conditions for clarity
+        // only 1 required but writing 3 conditions for clarity
         if ((a1 == a2) && (a2 == a3) && (a3 == a4)) {
             cout << "Square" << endl; return 0;
         }
