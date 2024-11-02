10294a51b250c876d3b30d1e836cf24308020957
sushantpadha <24b1057@iitb.ac.in>
2024-11-02 22:59:34 +0530

    update my-tour.cpp by optimizing a bit

--- 22/my-tour.cpp
+++ 22/my-tour.cpp
@@ -9,22 +9,59 @@
 using std::stack, std::cin, std::cout, std::cerr, std::endl, std::vector,
     std::unordered_set, std::ostream, std::string;
 
-#define __DBG_ENABLED 0
+/* Version 1.0 */
 
-#define LOG            \
-    if (__DBG_ENABLED) \
+/* Version 1.1 (minor optimizations)
+
+ * factor out the check for "if reachable position valid and not in visited", to
+ * hopefully improve speed a bit?
+
+ * also remove the generation code just hardcode values for reachable
+
+ * remove unnecessary getter methods by using `friend`
+
+ * cache these reachable calls
+*/
+
+#define __DBG_FLAG__ 0
+
+#define LOG           \
+    if (__DBG_FLAG__) \
     cerr
 
-#define DBG(x)         \
-    if (__DBG_ENABLED) \
-    cout << #x << "=" << x << " "
-#define nl             \
-    if (__DBG_ENABLED) \
-    cout << '\n'
+// macro for logging when debug is enabled
+#define DBG(x)          \
+    LOG << #x << " = "; \
+    PRINT_DBG(x);       \
+    LOG << " ";
+
+template <typename T>
+void __dbg__(const T& value) {
+    LOG << value;
+}
+
+template <typename T>
+void __dbg__(const vector<T>& vec) {
+    LOG << "[";
+    for (size_t i = 0; i < vec.size() - 1; ++i) {
+        __dbg__(vec[i]);
+        LOG << ", ";
+    }
+    __dbg__(vec[vec.size() - 1]);
+    LOG << "]";
+}
+#define NL LOG << '\n'
 
 #define TRACK LOG << __LINE__ << ": tour (" << tour.size() << ") = " << tour << endl;
 
+////////////////////////////////////////////////////////////////////////////////
+
 constexpr int BOARD_SIZE = 8;
+constexpr int MAX_BOARD_SIZE = 9;
+constexpr char START_POSITION[3] = "a1";
+const bool CACHE_REACHABLE_POSITIONS = true;
+
+////////////////////////////////////////////////////////////////////////////////
 
 // Valid positions [1, board_size] for both rank and file
 // Silently converts invalid positions to (0, 0) or "--" and bool(...) evaluates to
@@ -32,30 +69,24 @@ constexpr int BOARD_SIZE = 8;
 // false
 class Posn {
     // rank (4 in a4). varies 1 to bsize, or 0 if invalid
-    int row;
+    int row = 0;
     // file (a in a4). varies 1 to bsize, or 0 if invalid
-    int col;
-    string name;
-    int bsize;
-    bool isValid;
+    int col = 0;
+    string name = "--";
+    int bsize = BOARD_SIZE;
+    bool isValid = true;
 
-public:
-    Posn(int file = 0, int rank = 0, int boardsize = BOARD_SIZE)
-        : col(file), row(rank), bsize(boardsize), isValid(true), name("--") {
-        // check for invalid input
-        if (row > bsize || row < 1 || col > bsize || col < 1) {
-            row = col = 0;
-            isValid = false;
-        } else {
-            name.resize(2);  // Ensure the size is 2 before modifying characters
-            name[0] = (char)'a' - 1 + col;
-            name[1] = (char)'0' + row;
-        }
-    }
+    // flag to enable reachable positions caching
+    static bool cacheReachablePosns;
 
-    Posn(string s, int boardsize = BOARD_SIZE)
-        : Posn(validateString(s) ? s[0] - 'a' + 1 : 0, s[1] - '0', boardsize) {}
+    // flag for when cache initializing is on going
+    static bool initializingCache;
+    // for every bsize, for every row, for every col, the list of reachable posns
+    static vector<vector<vector<vector<Posn>>>> reachablePosnsCache;
+    // for every bsize whether the cache is initialized
+    static vector<bool> isCacheInitialized;
 
+    // TODO: highly oversimplified string validation
     // assuming 1 <= boardsize <= 9
     static bool validateString(const string& s) {
         if (s.size() != 2) {
@@ -66,122 +97,199 @@ public:
         return true;
     }
 
-    // return vector of valid, reachable positions
-    vector<Posn> reachable() {
-        vector<Posn> result;
+    // FILLING STATIC VARIABLES
+    // fill reachable positions for given boardsize
+    static void fillReachablePosnsBoard(int boardsize) {
+        if (isCacheInitialized[boardsize - 1])
+            return;
 
-        // generate all possible positions using 3 nested for loops
-        for (int i = 1; i <= 2; i++) {
-            for (int i_sgn = -1; i_sgn <= 1; i_sgn += 2) {
-                for (int j_sgn = -1; j_sgn <= 1; j_sgn += 2) {
-                    Posn p(col + i * i_sgn, row + (3 - i) * j_sgn);
-                    // keep only valid ones
-                    if (p)
-                        result.push_back(p);
-                }
+        for (int r = 1; r <= boardsize; r++) {
+            for (int c = 1; c <= boardsize; c++) {
+                fillReachablePosnsPosn(
+                    r, c, boardsize, reachablePosnsCache[boardsize - 1][r - 1][c - 1]);
             }
         }
 
+        isCacheInitialized[boardsize - 1] = true;
+    }
+
+    // fill reachable positions for given position (valid ones only)
+    static void fillReachablePosnsPosn(int row, int col, int boardsize,
+                                       vector<Posn>& list) {
+        if (!list.empty())
+            throw std::invalid_argument(
+                "list of reachable positions already computed. exiting.\n");
+
+        // hardcode for speed
+        constexpr int files[8] = {2, 2, -2, -2, 1, 1, -1, 1};
+        constexpr int ranks[8] = {1, -1, 1, -1, 2, -2, 2, -2};
+
+        // check all positions
+        for (int i = 0; i < 8; i++) {
+            const int file = col + files[i];
+            const int rank = row + ranks[i];
+            // keep only valid ones
+            if (file < 1 || file > boardsize || rank < 1 || rank > boardsize)
+                continue;
+            list.push_back({file, rank, boardsize});
+        }
+    }
+
+public:
+    // CONSTRUCTORS
+    // default constructor `file`, `rank` and `boardsize`
+    Posn(int file = 0, int rank = 0, int boardsize = BOARD_SIZE)
+        : col(file), row(rank), bsize(boardsize) {
+        // check for invalid input
+        if (row > bsize || row < 1 || col > bsize || col < 1) {
+            row = col = 0;
+            isValid = false;
+        }
+        // for valid input
+        else {
+            name.resize(2);
+            name[0] = (char)'a' - 1 + col;
+            name[1] = (char)'0' + row;
+        }
+        // ensure only one time cache initializing per recursive call
+        if (!initializingCache) {
+            initializingCache = true;
+            fillReachablePosnsBoard(boardsize);
+            initializingCache = false;
+        }
+    }
+
+    // initialize static variables
+    static void initializeStatic(bool cache = CACHE_REACHABLE_POSITIONS) {
+        cacheReachablePosns = cache;
+        reachablePosnsCache.resize(MAX_BOARD_SIZE);
+
+        for (int bsize = 1; bsize <= MAX_BOARD_SIZE; bsize++) {
+            reachablePosnsCache[bsize - 1] =
+                vector<vector<vector<Posn>>>(bsize, vector<vector<Posn>>(bsize));
+        }
+    }
+
+    // constructor using string for position and `boardsize`
+    Posn(string s, int boardsize = BOARD_SIZE)
+        : Posn(validateString(s) ? s[0] - 'a' + 1 : 0, s[1] - '0', boardsize) {}
+
+    // return vector of reachable positions for the instance
+    vector<Posn> reachable() const {
+        // if cache has been enabled and corressponding cache initialized, return from
+        // cache
+        if (cacheReachablePosns && isCacheInitialized[bsize - 1])
+            return reachablePosnsCache[bsize - 1][row - 1][col - 1];
+
+        // else compute it and return
+        vector<Posn> result;
+        fillReachablePosnsPosn(row, col, bsize, result);
         return result;
     }
 
-    // for our purposes, we assume board size will always be compatible
+    // required for hashing for unordered_set
+    // (for our purposes, we assume board size will always be compatible)
     bool operator==(const Posn& other) const {
         return row == other.row && col == other.col;
     }
 
-    const string& getName() const { return name; }
-    const int& getRow() const { return row; }
-    const int& getCol() const { return col; }
-
+    // returns whether position is valid
     operator bool() const { return isValid; }
 
     friend ostream& operator<<(ostream& out, const Posn& p);
+    friend class PosnHasher;
 };
 
-ostream& operator<<(ostream& out, const Posn& p) { return (out << p.getName()); }
+// define static members outside the class
+bool Posn::cacheReachablePosns = CACHE_REACHABLE_POSITIONS;
+bool Posn::initializingCache = false;
+vector<vector<vector<vector<Posn>>>> Posn::reachablePosnsCache;
+vector<bool> Posn::isCacheInitialized(MAX_BOARD_SIZE, false);
 
+ostream& operator<<(ostream& out, const Posn& p) { return (out << p.name); }
+
+// custom hasher class for storing visited posns in set
 // https://chatgpt.com/share/67252ba9-36bc-800d-9d4a-75de30df8e91
-struct PosnHasher {
+class PosnHasher {
+public:
     size_t operator()(const Posn& p) const {
-        return std::hash<int>{}(p.getRow()) ^ (std::hash<int>{}(p.getCol()) * 31);
+        return std::hash<int>{}(p.row) ^ (std::hash<int>{}(p.col) * 31);
     }
 };
 
+// Class for storing variables related to tour and implementing basic methods
 class Tour {
     vector<Posn> t_stack;
     unordered_set<Posn, PosnHasher> t_set;
     bool done;
-    int length;
+    int maxlength;
+    int bsize;
 
 public:
-    Tour() : t_stack({}), t_set({}), length(0), done(false) {}
+    // DEFAULT CONSTRUCTOR
+    Tour(int tour_length, int board_size)
+        : t_stack({}),
+          t_set({}),
+          maxlength(tour_length),
+          bsize(board_size),
+          done(false) {}
 
-    Tour(int tour_length, int board_size, string startposn)
-        : t_stack({startposn}), t_set({startposn}), length(tour_length), done(false) {}
+    // return const lref to last element in tour
+    const Posn& tail() const { return t_stack.back(); }
 
-    Posn& tail() { return t_stack.back(); }
+    // return length of tour traversed currently
+    int size() const { return t_stack.size(); }
 
-    int size() { return t_stack.size(); }
+    // return all positions reachable from tail position (including visited ones)
+    vector<Posn> reachable() const { return tail().reachable(); }
 
-    // return all positions reachable from tail position, not in visited
-    vector<Posn> reachable() {
-        vector<Posn> r = tail().reachable();
-        vector<Posn> result;
-        for (const Posn& pos : r) {
-            if (!isVisited(pos)) {
-                result.push_back(pos);
-            }
-        }
-
-        // LOG << tail() << " reachable -> ";
-        // for (auto p : result) LOG << p << " ";
-        // LOG << endl;
-
-        return result;
-    }
-
+    // return whether a position has been visited in current tour
     bool isVisited(const Posn& p) const { return t_set.find(p) != t_set.end(); }
 
-    // remove last visited posn
+    // remove last visited position
     void pop() {
         Posn p = tail();
         t_stack.pop_back();
         t_set.erase(p);
     }
 
+    // add a position to the tour
     // CAUTION: assumes `p` is not in tour
     void add(const Posn& p) {
         t_stack.push_back(p);
         t_set.insert(p);
 
         // check if desired tour length has been achieved
-        if (size() == length)
+        if (size() == maxlength)
             done = true;
     }
 
+    // return whether tour is empty
     bool empty() const { return t_stack.empty(); }
 
-    operator bool() { return done; }
-
-    const vector<Posn>& getTour() const { return t_stack; }
+    // return whether desired tour length has been achieved
+    operator bool() const { return done; }
 
     friend ostream& operator<<(ostream& out, const Tour& t);
 };
 
 ostream& operator<<(ostream& out, const Tour& t) {
-    for (const Posn& c : t.getTour()) {
+    for (const Posn& c : t.t_stack) {
         out << c << " ";
     }
     return out;
 }
 
+// perform recursive DFS on tour using backtracking
 void DFS(Tour& tour) {
     for (const Posn& p : tour.reachable()) {
         // base case
         if (tour)
             return;
+
         // reduce
+        if (tour.isVisited(p))
+            continue;
         tour.add(p);
         TRACK;
 
@@ -196,8 +304,11 @@ void DFS(Tour& tour) {
     }
 }
 
+// find the tour of `length` with `board_size` from `startposn`
 Tour find_tour(int length, int board_size, string startposn) {
-    Tour tour(length, board_size, startposn);
+    Tour tour(length, board_size);
+
+    tour.add(startposn);
 
     DFS(tour);
 
@@ -207,30 +318,41 @@ Tour find_tour(int length, int board_size, string startposn) {
 int main(int argc, char** argv) {
     // take command line arguments
     int M = 25, N = BOARD_SIZE;
-    string startposn = "a1";
-    if (argc > 4) {
+    bool cacheReachablePosns = CACHE_REACHABLE_POSITIONS;
+    string startposn = START_POSITION;
+
+    if (argc > 5) {
         std::cerr << "Usage: " << argv[0] << " [tour-length = " << M
                   << "] [board-size = " << N << "] [start-position = " << startposn
-                  << "]" << std::endl;
+                  << "] [cache-reachable-posns = "
+                  << (CACHE_REACHABLE_POSITIONS ? "1" : "0") << "]" << std::endl;
         exit(1);
     }
     if (argc >= 2)
         M = std::stoi(argv[1]);
     if (argc >= 3)
         N = std::stoi(argv[2]);
-    if (argc == 4) {
+    if (argc >= 4) {
         startposn[0] = argv[3][0];
         startposn[1] = argv[3][1];
     }
+    if (argc == 5) {
+        cacheReachablePosns = std::stoi(argv[4]);
+    }
+
+    // initialize static variables
+    Posn::initializeStatic(cacheReachablePosns);
 
     // find tour
     Tour tour = find_tour(M, N * N, startposn);
 
+    // print tour
     if (!tour) {
         cerr << "Tour not found!\n";
         return 1;
     }
 
     cout << tour << endl;
+
     return 0;
 }
